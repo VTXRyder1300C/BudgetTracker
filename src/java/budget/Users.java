6f78cca38665685397198afae58537d9e456c12b
Steven Neugent <steven.neugent@outlook.com>
2020-09-18 23:19:22 -0400

    tuning does this work

--- src/java/budget/Users.java
+++ src/java/budget/Users.java
@@ -27,11 +27,72 @@ public class Users implements Serializable {
     private static String dbURL = "jdbc:derby://localhost:1527/contact;create=true;user=nbuser;password=nbuser";
     private static String usersTable = "APP.Users";
     private static String incomeTable = "APP.Income";
-    private static String expenseTable = "APP.Expenses"; 
+    private static String expenseTable = "APP.Expenses";
     private String test = "lets try something";
     // jdbc Connection
     private static Connection conn = null;
     private static Statement stmt = null;
+
+    /**
+     * Our main function that will handle the table values. The main logic
+     * should go through our switch statement and each block of code should have
+     * its own self contained function
+     *
+     * @param t - string of type of thing to do.
+     */
+    public void setTable(String t) {//t holds the value that is passed in
+        switch (t.toLowerCase()) {
+            case "login":
+                this.handleLogin();
+                break;
+            default:
+                System.err.println("Unknown value");
+        }
+
+    }
+
+    private boolean isValidLogin(String userName, String password) {
+        return this.username.equalsIgnoreCase(userName) && this.password.equals(password);
+    }
+    /**
+     * Handle our login.  
+     * 
+     * TODO: Move connection, derby or otherwise to the top of the program.
+     */
+    private void handleLogin() {
+        this.loginAttempted = true;
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            //Get a connection
+            conn = DriverManager.getConnection(dbURL);
+        } catch (Exception except) {
+            except.printStackTrace();
+        }
+        try {//connect to the database
+            //maybe have a saved statement rather than creating it on the fly each time?
+            stmt = conn.createStatement();
+            try (ResultSet results = stmt.executeQuery("select * from " + usersTable)) {
+                while (results.next()) {
+                    String userID = results.getString(1);
+                    String username = results.getString(2);
+                    String password = results.getString(3);
+                    String name = results.getString(4);
+                    if (this.isValidLogin(username, password)) {
+                        this.userID = userID;
+                        this.username = username;
+                        this.name = name;
+                        break;//kill the loop after a valid user is found
+                    } else if (this.username.equalsIgnoreCase(username)) {
+                        System.out.println("User exists, but password is wrong...");//test only see glassfish server output
+                    }
+                }
+            }
+            stmt.close();
+        } catch (SQLException sqlExcept) {
+            //it yelled at me about the stack trace
+            System.err.println(sqlExcept.toString());
+        }
+    }
 
     //information sent from the page to update the java page
     public void setUserID(String userID) {
@@ -50,9 +111,7 @@ public class Users implements Serializable {
         this.password = password;
     }
 
-
     //transactions
-    
     //Requests for the information from the HMTL page
     public String getTest() {
         return test;
@@ -67,7 +126,6 @@ public class Users implements Serializable {
     }
 
     //transactions
-    
     public String getUsername() {
         return username;
     }
@@ -75,7 +133,6 @@ public class Users implements Serializable {
     public String getPassword() {
         return password;
     }
-
 
     public Boolean getPasswordMismatch() {
         return passwordMismatch;
@@ -89,43 +146,4 @@ public class Users implements Serializable {
         return table;
     }
 
-    public void setTable(String t) {//t holds the value that is passed in
-        if ("login".equalsIgnoreCase(t)) {
-            System.out.println("Logging in...");//test only see glassfish server output
-            this.loginAttempted = true;
-            try {
-                Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
-                //Get a connection
-                conn = DriverManager.getConnection(dbURL);
-            } catch (Exception except) {
-                except.printStackTrace();
-            }
-            try {//connect to the database
-                System.out.println("Connecting to database for: " + t);//test only see glassfish server output
-                stmt = conn.createStatement();
-                ResultSet results = stmt.executeQuery("select * from " + usersTable);
-                while (results.next()) {
-                    String userID = results.getString(1);
-                    String username = results.getString(2);
-                    String password = results.getString(3);
-                    String name = results.getString(4);
-                    if (this.username.equalsIgnoreCase(username) && this.password.equals(password)) {
-                        this.userID = userID;
-                        this.username = username;
-                        this.name = name;
-                        System.out.println("User exists and passwords match!");//test only see glassfish server output
-                        break;//kill the loop after a valid user is found
-                    } else if (this.username.equalsIgnoreCase(username)) {
-                        System.out.println("User exists, but password is wrong...");//test only see glassfish server output
-                    }
-                }
-                results.close();
-                stmt.close();
-            } catch (SQLException sqlExcept) {
-                sqlExcept.printStackTrace();
-            }
-        }  else {
-            System.out.println("Error... nothing caught in setTable... Please ensure proper case of operation.  Such as(insertExpense..etc");//test only see glassfish server output
-        }
-    }
 }
